17aef60a47bc0989068d6c32779cc423d43fe391
fusiyi <fusiyi11@163.com>
2022-12-02 14:19:18 +0800

    3.2 修改角色

--- src/main.js
+++ src/main.js
@@ -8,6 +8,7 @@ import './assets/styles/index.scss'
 import request from '@/utils/request'
 import '@/assets/icons/index'
 import permission from "@/components/Permission";
+import 'default-passive-events' //解决新版的Chrome浏览器警告
 
 Vue.config.productionTip = false
 Vue.use(permission)
